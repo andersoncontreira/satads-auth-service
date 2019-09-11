90298a9ed531ca890364a1c3b1c1f16bc727ad67
Anderson de Oliveira Contreira <anderson.contreira@gmai.com>
2019-09-10 23:56:05 -0300

    routes fixes

--- app/http/routes/v1/auth-routes.js
+++ app/http/routes/v1/auth-routes.js
@@ -5,10 +5,10 @@ module.exports = {
     const version = 'v1'
     const versionPath = '/' + version
 
-    router.post(process.env.ROOT_PATH + versionPath + '/auth/signin', (request, response) => AuthController.signIn(request, response))
-    router.post(process.env.ROOT_PATH + versionPath + '/auth/signup', (request, response) => AuthController.signUp(request, response))
-    router.post(process.env.ROOT_PATH + versionPath + '/auth/recover', (request, response) => AuthController.recover(request, response))
-    router.post(process.env.ROOT_PATH + versionPath + '/auth/recover/callback', (request, response) => AuthController.recoverCallback(request, response))
+    router.post(process.env.ROOT_PATH + versionPath + '/signin', (request, response) => AuthController.signIn(request, response))
+    router.post(process.env.ROOT_PATH + versionPath + '/signup', (request, response) => AuthController.signUp(request, response))
+    router.post(process.env.ROOT_PATH + versionPath + '/recover', (request, response) => AuthController.recover(request, response))
+    router.post(process.env.ROOT_PATH + versionPath + '/recover/callback', (request, response) => AuthController.recoverCallback(request, response))
 
     return router
   }
